c82768985c0a3586f5add4ef0d8b6575766641ea
weeedushi <bhardwajvidushi85@gmail.com>
2025-07-19 01:18:12 +0530

    Pattern 3 solved

--- Patterns/Pattern1.cpp
+++ Patterns/Pattern1.cpp
@@ -18,10 +18,20 @@ void pattern2(int n){
         cout<<endl;
     }
 }
+
+void pattern3(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cin>>n;
 
     pattern1(n);
     pattern2(n);
+    pattern3(n);
 }
