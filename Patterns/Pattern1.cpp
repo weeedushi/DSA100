f7bd3efbf5318d05761cf30a7373146ea8a7a909
weeedushi <bhardwajvidushi85@gmail.com>
2025-07-19 01:19:56 +0530

    Pattern 4 solved

--- Patterns/Pattern1.cpp
+++ Patterns/Pattern1.cpp
@@ -27,6 +27,15 @@ void pattern3(int n){
         cout<<endl;
     }
 }
+
+void pattern4(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<i+1<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cin>>n;
@@ -34,4 +43,5 @@ int main(){
     pattern1(n);
     pattern2(n);
     pattern3(n);
+    pattern4(n);
 }
