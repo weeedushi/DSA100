ef04837225f9bf2a6b1d16379ae599d61ad90161
weeedushi <bhardwajvidushi85@gmail.com>
2025-07-19 01:27:58 +0530

    Pattern 5 solved

--- Patterns/Pattern1.cpp
+++ Patterns/Pattern1.cpp
@@ -36,6 +36,16 @@ void pattern4(int n){
         cout<<endl;
     }
 }
+
+void pattern5(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     int n;
     cin>>n;
@@ -44,4 +54,5 @@ int main(){
     pattern2(n);
     pattern3(n);
     pattern4(n);
+    pattern5(n);
 }
