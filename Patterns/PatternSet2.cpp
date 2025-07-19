06b42e6ffec352b9b9886061a7fc5baccdda38a2
weeedushi <bhardwajvidushi85@gmail.com>
2025-07-19 12:30:32 +0530

    Pattern set 2 solved

--- Patterns/PatternSet2.cpp
+++ Patterns/PatternSet2.cpp
@@ -37,9 +37,22 @@ void pattern1_method2(int n){
     }
 }
 
+void pattern2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     int n;
     cin>>n;
     pattern1_method1(n);
     pattern1_method2(n);
+    pattern2(n);
 }
---
+++ Patterns/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+pattern1_method1(n);
+    pattern1_method2(n);
