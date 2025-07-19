d69ee7deca3f03ed02d037a246aa3e76fa22acb8
weeedushi <bhardwajvidushi85@gmail.com>
2025-07-19 13:10:38 +0530

    Pattern set 2 solved

--- Patterns/PatternSet2.cpp
+++ Patterns/PatternSet2.cpp
@@ -7,9 +7,25 @@ using namespace std;
 //   ***** 
 //  ******* 
 
+// Pattern 2
+//  ******* 
+//   ***** 
+//    ***
+//     *  
+
+// Pattern 3
+//     *    
+//    ***
+//   ***** 
+//  ******* 
+//  ******* 
+//   ***** 
+//    ***
+//     *  
+
 void pattern1_method1(int n){
     for(int i=0;i<n;i++){
-        for(int j=0;j<n-i;j++){
+        for(int j=0;j<n-i-1;j++){
             cout<<" ";
         }
         for(int k=0;k<=i;k++){
@@ -49,10 +65,60 @@ void pattern2(int n){
     }
 }
 
+void pattern3(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*n-(2*i+1);j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<"Thankyou everyone"<<endl;
+}
+
+void pattern4_method1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern4_method2(int n){
+    for(int i=1;i<=2*n-1;i++){
+        int stars=i;
+        if(i>n) stars=2*n-i;
+        for(int j=1;j<=stars;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n;
     cin>>n;
     pattern1_method1(n);
     pattern1_method2(n);
     pattern2(n);
+    pattern3(n);
+    pattern4_method1(n);
+    pattern4_method2(n);
 }
