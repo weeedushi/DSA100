72acd0beeca8f5aa23f330b07bb03739542aeffe
weeedushi <bhardwajvidushi85@gmail.com>
2025-07-19 11:58:44 +0530

    Patternset 2 solved

---
+++ Patterns/PatternSet2.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Pattern 1
+//     *    
+//    ***
+//   ***** 
+//  ******* 
+
+void pattern1_method1(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<" ";
+        }
+        for(int k=0;k<=i;k++){
+            cout<<"*";
+        }
+        for(int l=0;l<i;l++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+}
+
+void pattern1_method2(int n){
+    for(int i=0;i<n;i++){
+        //space
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //stars
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    pattern1_method1(n);
+    pattern1_method2(n);
+}
