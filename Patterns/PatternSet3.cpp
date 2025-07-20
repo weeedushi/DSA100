7cabe9f6d71374022efb22253f0e772acf25d273
weeedushi <bhardwajvidushi85@gmail.com>
2025-07-20 14:04:00 +0530

    Pattern set 3 solved

--- Patterns/PatternSet3.cpp
+++ Patterns/PatternSet3.cpp
@@ -15,9 +15,25 @@ void pattern1(int n){
 
 }
 
+void pattern2(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<j+1;
+        }
+        for(int j=1;j<=2*(n-i-1);j++){
+            cout<<" ";
+        }
+        for(int j=i;j>=0;j--){
+            cout<<j+1;
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     int n;
     cin>>n;
 
     pattern1(n);
+    pattern2(n);
 }
